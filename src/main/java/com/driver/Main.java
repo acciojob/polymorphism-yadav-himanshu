57b4216d112ddb1859b104ad821a794c0974c440
Himanshu Yadav <himanshuyadav7852@gmail.com>
2023-01-22 20:20:32 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(3,4));
+        System.out.println(p.product(3,4,6));
+        System.out.println(p.product(3.5,4.5));
+    }
 }
